dd931cfbef617684ed55975548ae8daae9331d9c
Jordan Hubscher <jordanhubscher@gmail.com>
2018-10-28 00:13:52 -0400

    Struggled with making a NON-blocking socket that kept throwing an error after detecting no data, so just shoved it in a while loop, man I can't think straight.

--- httpc/httpc.py
+++ httpc/httpc.py
@@ -5,16 +5,17 @@ from urllib3 import util
 
 @click.group()
 def cli():
-    """httpc is a curl-like application but supports HTTP protocol only."""
+    """httpc is a curl-like application but supports the HTTP protocol."""
     pass
 
 
 @cli.command()
 @click.option("-v", is_flag=True, help="Prints the detail of the response such as protocol, status, and headers.")
 @click.option("-h", multiple=True, help="Associates headers to HTTP Request with the format 'key:value'.")
-@click.argument('url', type=str, required=True)
+@click.argument("url", type=str, required=True)
 def get(url, v, h):
     """Get executes a HTTP GET request for a given URL."""
+
     conn = http.client.HTTPConnection(util.parse_url(url).host)
 
     # Stores the headers received from the command line in a dictionary
@@ -39,10 +40,10 @@ def get(url, v, h):
 @click.option("-h", multiple=True, help="Associates headers to HTTP Request with the format 'key:value'.")
 @click.option("-d", type=str, help="Associates an inline data to the body HTTP POST request.")
 @click.option("-f", type=click.File('r'), help="Associates the content of a file to the body HTTP POST request.")
-@click.argument('url', type=str, required=True)
+@click.argument("url", type=str, required=True)
 def post(url, v, h, d, f):
-    """Post executes a HTTP POST request for a given URL with inline data or from
-    file."""
+    """Post executes a HTTP POST request for a given URL with inline data or from file."""
+
     conn = http.client.HTTPConnection(util.parse_url(url).host)
 
     # Stores the headers received from the command line in a dictionary
--- httpfs/httpfs.py
+++ httpfs/httpfs.py
@@ -1,5 +1,67 @@
+import click
 import socket
 import threading
 import os
+import sys
+import errno
+import time
 
 
+@click.command()
+@click.option("-v", is_flag=True, help="Prints debugging messages")
+@click.option(
+    "-p",
+    is_flag=True,
+    type=int,
+    default=8080,
+    help="Specifies the port number that the server will listen and server at."
+    "Default is 8080."
+)
+@click.option(
+    "-d",
+    is_flag=True,
+    type=str,
+    default="./",
+    help="Specifies the directory when launching the application."
+    "Default is the current directory."
+)
+def httpfs(v, p, d):
+    """
+    httpfs is a simple file server.
+
+    usage: httpfs [-v] [-p PORT] [-d PATH-TO-DIR]
+    """
+
+    data = ""
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setblocking(False)
+    serverSocket.bind((socket.gethostname(), p))
+    serverSocket.listen(1)
+
+    while True:
+        try:
+            (clientSocket, address) = serverSocket.accept()
+            print("Connected to: %s", address)
+
+            while True:
+                buffer = clientSocket.recv(1024)
+                if not data:
+                    break
+                else:
+                    data += "%s%s" % (data, buffer)
+                    
+            print("data received: %s", data)
+        except BlockingIOError as e:
+            if e.args[0] == errno.EAGAIN or e.args[0] == errno.EWOULDBLOCK:
+                print("No data available")
+                time.sleep(3)
+            else:
+                # a "real" error occurred
+                print(e)
+                sys.exit(1)
+
+    clientSocket.close()
+    serverSocket.close()
+
+if __name__ == '__main__':
+    httpfs()
