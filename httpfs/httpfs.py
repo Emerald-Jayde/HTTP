c0a10d4383cc00cc69173320054f277aa1ab5443
Jordan Hubscher <jordanhubscher@gmail.com>
2018-10-31 16:01:15 -0400

    Added appropriate responses for GET and POST requests: 1. When file does not exist. 2. When files cannot be opened/written to/read from. Otherwise, GET returns file contents and POST writes to said file or creates it with its body contents.

--- httpfs/httpfs.py
+++ httpfs/httpfs.py
@@ -10,6 +10,7 @@ from pathlib import Path
 CRLF = "\r\n"
 path_to_dir = "./wwwroot"
 
+
 @click.command()
 @click.option("-v", is_flag=True, help="Prints debugging messages")
 @click.option(
@@ -49,7 +50,8 @@ def httpfs(v, p, d):
 
         while True:
             (client_socket, address) = server_socket.accept()
-            threading.Thread(target=handle_client_connection, args=(client_socket, address)).start()
+            threading.Thread(target=handle_client_connection,
+                             args=(client_socket, address)).start()
     except OSError as e:
         print(e)
         sys.exit(1)
@@ -63,72 +65,84 @@ def handle_client_connection(client_socket, client_address):
     msg_rcvd = ""
     raw_msg_rcvd = ""
 
-    try:
-        while True:
-            buffer = client_socket.recv(1024)
-            if not buffer:
-                break
-            else:
-                msg_rcvd += str(buffer.decode("utf-8"))
-                raw_msg_rcvd += buffer
+    while True:
+        buffer = client_socket.recv(1024)
+        if not buffer:
+            break
+        else:
+            msg_rcvd += str(buffer.decode("utf-8"))
+            raw_msg_rcvd += buffer
 
-        print(
-            "Data received from client ",
-            client_address,
-            ":",
-            nonlocal CRLF,
-            msg_rcvd,
-            end="\n"
-        )
+    print(
+        "Data received from client ",
+        client_address,
+        ":",
+        nonlocal CRLF,
+        msg_rcvd,
+        end="\n"
+    )
 
-        # Splits client message by CRLF
-        msg_rcvd_lines = msg_rcvd.splitlines()
+    # Splits client message by CRLF
+    msg_rcvd_lines = msg_rcvd.splitlines()
 
-        # Extract request line
-        msg_rcvd_request_line = msg_rcvd_lines()[0].split(" ")
-        # Extract HTTP verb
-        verb = request_line[0]
-        # Extract URL/path
-        path = Path(request_line[1])
-        # Extract HTTP version
-        protocol_version = request_line[2]
+    # Extract request line
+    msg_rcvd_request_line = msg_rcvd_lines()[0].split(" ")
+    # Extract HTTP verb
+    verb = request_line[0]
+    # Extract URL/path
+    path = Path(request_line[1])
+    # Extract HTTP version
+    protocol_version = request_line[2]
 
-        # Extract headers
-        msg_rcvd_headers = [
-            header for header in msg_rcvd_lines[1:] if(":" in header)
+    # Extract headers
+    msg_rcvd_headers = [
+         header for header in msg_rcvd_lines[1:] if(":" in header)
+    ]
+
+     # Extract message body
+     msg_rcvd_body = "\n".join(
+        [
+            msg_body_line
+            for msg_body_line in msg_rcvd_lines[1:]
+                if(":" not in header)
         ]
+    )
 
-        # Extract message body
-        msg_rcvd_body = "\n".join(
-            [
-                msg_body_line
-                for msg_body_line in msg_rcvd_lines[1:]
-                    if(":" not in header)
-            ]
-        )
+      invalid_path_vals = ["sudo", "su", "-i", "..", "rm", "-r", "-f"]
 
-        invalid_path_vals = ["sudo", "su", "-i", "..", "rm", "-r", "-f"]
-
-        if invalid_path_vals in path:
-            client_socket.sendall(protocol_version + " 401 Unauthorized" + 3 * nonlocal CRLF)
+       if invalid_path_vals in path:
+            client_socket.sendall(
+                protocol_version +
+                " 401 Unauthorized" +
+                3 * nonlocal CRLF
+            )
         else:
             if verb is "GET":
                 if not path.exists():
-                    client_socket.sendall(protocol_version + " 400 Bad Request" + 3 * nonlocal CRLF)
+                    client_socket.sendall(
+                        protocol_version +
+                        " 400 Bad Request"+
+                        3 * nonlocal CRLF
+                    )
                 if path.is_file():
                     try:
                         file = open(nonlocal path_to_dir + path.__str__(), "rb")
 
                         resp_line = protocol_version + " 200 OK" + nonlocal CRLF
-                        headers = "Content-Length:" + os.path.getsize(path.__str__()) + nonlocal CRLF
+                        headers = "Content-Length:" +
+                            os.path.getsize(path.__str__()) +
+                            nonlocal CRLF
                         msg_body = file.readall() + CRLF
                         client_socket.sendall(resp_line + headers + msg_body)
 
                         print("", end="\n")
                     except OSError as e:
-                        resp_line = protocol_version + " 500 Internal Server Error" + nonlocal CRLF
+                        resp_line = protocol_version +
+                            " 500 Internal Server Error" +
+                            nonlocal CRLF
                         headers = nonlocal CRLF
-                        msg_body = "File could not be opened/read from." + nonlocal CRLF
+                        msg_body = "File could not be opened/read from." +
+                            nonlocal CRLF
                         client_socket.sendall(resp_line + headers + msg_body)
 
                         print(e, end="\n")
@@ -144,25 +158,25 @@ def handle_client_connection(client_socket, client_address):
                     msg_body = nonlocal CRLF
                     client_socket.sendall(resp_line + headers + msg_body)
                 except OSError as e:
-                    resp_line = protocol_version + " 500 Internal Server Error" + nonlocal CRLF
+                    resp_line = protocol_version +
+                        " 500 Internal Server Error" +
+                        nonlocal CRLF
                     headers = nonlocal CRLF
-                    msg_body = "File could not be opened/written to." + nonlocal CRLF
+                    msg_body = "File could not be opened/written to." +
+                        nonlocal CRLF
                     client_socket.sendall(resp_line + headers + msg_body)
 
                     print(e, end="\n")
                 finally:
                     file.close()
             else:
-                resp_line = protocol_version + " 503 Service Unavilable" + nonlocal CRLF
+                resp_line = protocol_version +
+                    " 503 Service Unavilable" +
+                    nonlocal CRLF
                 headers = nonlocal CRLF
                 msg_body = nonlocal CRLF
                 client_socket.sendall(resp_line + headers + msg_body)
 
-        # Get: look for it
-        # Post: create
-        # Validate the path
-
-    finally:
         client_socket.close()
 
 
